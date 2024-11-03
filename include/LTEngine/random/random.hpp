96b28add5143df4cdd09c9f6dd28e12ad20b4e2c
LTecher Offical <ltecheroffical@gmail.com>
2024-11-03 02:08:39 -0500

    refactor: Add more random

--- include/LTEngine/random/random.hpp
+++ include/LTEngine/random/random.hpp
@@ -9,40 +9,18 @@ namespace LTEngine::Random {
     public:
         virtual ~Random() = 0;
 
-        virtual void seed(u8 seed);
-
         virtual u8 next_u8();
-        virtual u8 next_u8(u8 min, u8 max);
-        virtual u8 next_u8(u8 min);
         virtual u16 next_u16();
-        virtual u16 next_u16(u16 min, u16 max);
-        virtual u16 next_u16(u16 min);
         virtual u32 next_u32();
-        virtual u32 next_u32(u32 min, u32 max);
-        virtual u32 next_u32(u32 min);
         virtual u64 next_u64();
-        virtual u64 next_u64(u64 min, u64 max);
-        virtual u64 next_u64(u64 min);
 
         virtual i8 next_i8();
-        virtual i8 next_i8(i8 min, i8 max);
-        virtual i8 next_i8(i8 min);
         virtual i16 next_i16();
-        virtual i16 next_i16(i16 min, i16 max);
-        virtual i16 next_i16(i16 min);
         virtual i32 next_i32();
-        virtual i32 next_i32(i32 min, i32 max);
-        virtual i32 next_i32(i32 min);
         virtual i64 next_i64();
-        virtual i64 next_i64(i64 min, i64 max);
-        virtual i64 next_i64(i64 min);
 
         virtual f32 next_f32();
-        virtual f32 next_f32(f32 min, f32 max);
-        virtual f32 next_f32(f32 min);
         virtual f64 next_f64();
-        virtual f64 next_f64(f64 min, f64 max);
-        virtual f64 next_f64(f64 min);
 
         virtual void nextBytes(u8 *data, u32 length);
     };
---
+++ src/random/unix_random.cpp
@@ -0,0 +1,104 @@
+#include <limits>
+
+#include <LTEngine/random/unix_random.hpp>
+
+
+using namespace LTEngine::Random;
+
+
+UnixRandom::UnixRandom(u32 bufferSize) {
+    m_bufferSize = bufferSize;
+
+    m_urandom.open("/dev/urandom", std::ios::in | std::ios::binary);
+    if (!m_urandom.is_open()) {
+        throw std::runtime_error("Failed to open /dev/urandom");
+    }
+
+    m_buffer.resize(m_bufferSize);
+
+    m_urandom.read(reinterpret_cast<char*>(&m_buffer[0]), m_bufferSize);
+}
+
+UnixRandom::~UnixRandom() {
+    m_urandom.close();
+}
+
+
+u8 UnixRandom::next_u8() {
+    if (m_buffer.size() <= 0) {
+        m_buffer.resize(m_bufferSize);
+        m_urandom.read(reinterpret_cast<char*>(&m_buffer[0]), m_bufferSize);
+    }
+
+    u8 value = m_buffer.back();
+    m_buffer.pop_back();
+    return value;
+}
+
+u16 UnixRandom::next_u16() {
+    if (m_buffer.size() <= 0) {
+        m_buffer.resize(m_bufferSize);
+        m_urandom.read(reinterpret_cast<char*>(&m_buffer[0]), m_bufferSize);
+    }
+
+    u16 value = m_buffer.back() | m_buffer.back();
+    m_buffer.pop_back();
+    m_buffer.pop_back();
+    return value;
+}
+
+u32 UnixRandom::next_u32() {
+    if (m_buffer.size() <= 0) {
+        m_buffer.resize(m_bufferSize);
+        m_urandom.read(reinterpret_cast<char*>(&m_buffer[0]), m_bufferSize);
+    }
+
+    u32 value = m_buffer.back() | m_buffer.back() | m_buffer.back() | m_buffer.back();
+    for (int i = 0; i < sizeof(u32); i++) {
+        m_buffer.pop_back();
+    }
+    return value;
+}
+
+u64 UnixRandom::next_u64() {
+    if (m_buffer.size() <= 0) {
+        m_buffer.resize(m_bufferSize);
+        m_urandom.read(reinterpret_cast<char*>(&m_buffer[0]), m_bufferSize);
+    }
+
+    u64 value = m_buffer.back() | m_buffer.back() | m_buffer.back() | m_buffer.back() | m_buffer.back() | m_buffer.back() | m_buffer.back() | m_buffer.back();
+    for (int i = 0; i < sizeof(u64); i++) {
+        m_buffer.pop_back();
+    }
+    return value;
+}
+
+
+i8 UnixRandom::next_i8() {
+    u8 value = next_u8();
+    return *reinterpret_cast<i8*>(&value);
+}
+
+i16 UnixRandom::next_i16() {
+    u16 value = next_u16();
+    return *reinterpret_cast<i16*>(&value);
+}
+
+i32 UnixRandom::next_i32() {
+    u32 value = next_u32();
+    return *reinterpret_cast<i32*>(&value);
+}
+
+i64 UnixRandom::next_i64() {
+    u64 value = next_u64();
+    return *reinterpret_cast<i64*>(&value);
+}
+
+
+f32 UnixRandom::next_f32() {
+    return static_cast<f32>(next_u16()) / static_cast<f32>(std::numeric_limits<u16>::max());
+}
+
+f64 UnixRandom::next_f64() {
+    return static_cast<f64>(next_u32()) / static_cast<f64>(std::numeric_limits<u32>::max());
+}
