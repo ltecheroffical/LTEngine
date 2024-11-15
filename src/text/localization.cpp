fb195e6c705bcfff63cefd49f3e44351ed138a5e
LTecher Offical <ltecheroffical@gmail.com>
2024-11-14 20:12:51 -0500

    Add localization

---
+++ src/text/localization.cpp
@@ -0,0 +1,22 @@
+#include <LTEngine/text/localization.hpp>
+
+
+using namespace LTEngine;
+using namespace LTEngine::Text;
+
+
+void Localization::addMapping(Language lang, std::string path, std::string localizedText) {
+	m_textPaths[std::make_pair(lang, path)] = localizedText;
+}
+
+bool Localization::doesMappingExist(Language lang, std::string path) {
+	return m_textPaths.find(std::make_pair(lang, path)) != m_textPaths.end();
+}
+
+
+std::string Localization::getLocalized(Language lang, std::string path) {
+	if (!doesMappingExist(lang, path)) {
+		return path; // Very simple fallback, better than throwing an exception
+	}
+	return m_textPaths.at(std::make_pair(lang, path));
+}
