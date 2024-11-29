57f235a053039ad55bf208dd2607df5b157cef5d
LTecher Offical <ltecheroffical@gmail.com>
2024-11-29 10:37:10 -0500

    Add LTEngine size conversions

---
+++ include/LTEngine/common/data_size.h
@@ -0,0 +1,14 @@
+#ifndef _LTENGINE_DATA_SIZE_H_
+#define _LTENGINE_DATA_SIZE_H_
+
+#define LTENGINE_KB(n) ((n) * 1024ULL)
+#define LTENGINE_MB(n) ((n) * (1024ULL * 1024))
+#define LTENGINE_GB(n) ((n) * (1024ULL * 1024 * 1024))
+#define LTENGINE_TB(n) ((n) * (1024ULL * 1024 * 1024 * 1024))
+
+#define LTENGINE_KiB(n) ((n) * 1000ULL)
+#define LTENGINE_MiB(n) ((n) * (1000ULL * 1000))
+#define LTENGINE_GiB(n) ((n) * (1000ULL * 1000 * 1000))
+#define LTENGINE_TiB(n) ((n) * (1000ULL * 1000 * 1000 * 1000))
+
+#endif
